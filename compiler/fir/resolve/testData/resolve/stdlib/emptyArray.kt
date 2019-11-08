27c55a8f1c486a651e7ff661c1fcba897f20e530
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-11-08 13:07:46 +0300

    Cone conflict resolver: filter equivalent top-level callables

--- compiler/fir/resolve/testData/resolve/stdlib/emptyArray.kt
+++ compiler/fir/resolve/testData/resolve/stdlib/emptyArray.kt
@@ -1,9 +1,9 @@
-val x: Array<String> = <!AMBIGUITY!>emptyArray<!>()
+val x: Array<String> = emptyArray()
 
 val y: Array<String>
-    get() = <!AMBIGUITY!>emptyArray<!>()
+    get() = emptyArray()
 
 interface My
 
 val z: Array<out My>
-    get() = <!AMBIGUITY!>emptyArray<!>()
+    get() = emptyArray()
